ddaa0e34ec0d714614b2fd23478fbcb3023a3d8c
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-09-16 09:50:44 +0545

    .. Bootstrap Implementation.

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,1 +1,16 @@
-<h1>Home page view</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?> | <?=APP_NAME?></title>
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <h1>Home page view</h1>
+    
+    
+    <script src="<?=ROOT?>/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
