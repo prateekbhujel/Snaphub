256c213c184dd6057510c325e473275198915514
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-09-18 08:15:48 +0545

    nav Bar.

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?> | <?=APP_NAME?></title>
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/bootstrap.min.css">
-</head>
-<body>
+<?php $this->view('includes/header', $data); ?>
     
-    <h1>Home page view</h1>
+
+
+
+<?php $this->view('includes/footer', $data); ?>
     
-    
-    <script src="<?=ROOT?>/assets/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
