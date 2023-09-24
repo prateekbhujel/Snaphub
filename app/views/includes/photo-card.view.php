7cb6a6019ecaaf1d7a9404069f691d439ce3a866
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-09-24 16:08:29 +0545

    301. Testing

--- app/views/includes/photo-card.view.php
+++ app/views/includes/photo-card.view.php
@@ -3,4 +3,4 @@
 						<img src="<?=get_image($image->getThumbnail($row->image, 251, 250));?>" class="img-thumbnail" sstyle="object-fit: cover;width: 250px; height: 250px;">
 						<div class="card-header"> <?=esc($row->title); ?> </div>
 					</a>
-				</div>
+				</div>Test
